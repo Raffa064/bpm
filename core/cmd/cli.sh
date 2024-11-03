9f12ce1e6a10d859efda256ffb79229294860c8a
Raffa064 <raffaddtg@gmail.com>
2024-11-03 11:52:35 -0300

    Added more features for leak-test
    It can now receive a command to run before testing

--- core/cmd/cli.sh
+++ core/cmd/cli.sh
@@ -27,10 +27,39 @@ function cmd/version() {
 }
 
 function cmd/leak-test() {
-  local val="$1"
+  # bpm leak-test query -> print value of a global var
+  # bpm leak-test query command [-f (arg, ...)]-> print value of a global var after running command
+  # bpm leak-test -> print value of all global vars and functions
+  # bpm leak-test "" command [-f (arg, ...)]-> print value of all global vars and functions after running command
+  
+  local query
+  local command
+  local params=( "query" "command" )
 
-  if [ -n "$val" ]; then
-    echo "${!val-Not found $val}"
+  local i=0
+  for arg in "$@"; do
+    shift
+    if [ "$arg" == "-f" ]; then
+      break;
+    fi
+    
+    local param="${params[$i]}"
+    if [ -n "$param" ]; then
+      eval "${param}=\"$arg\""
+    else
+      break
+    fi
+
+    ((i++))
+  done
+
+  if [ -n "$command" ]; then
+    command="cmd/$command"
+    $command $@
+  fi
+
+  if [ -n "$query" ]; then
+    echo "[LEAK]: ${!query-Not found $query}"
     return
   fi
 
